4cb893c11e22ec1d589414d4a4da63bc378df848
Muhammad Hasnain <mhasnaindev@gmail.com>
2021-05-31 09:23:22 +0500

    style basic footer layout and add whitespace

--- footer.php
+++ footer.php
@@ -1,23 +1,24 @@
   </main>
 
   <footer class="footer">
-    <div class="container container--large clearfix">
+    <div class="theme-container theme-container--large">
       <hr />
-      <nav class="navigation margin-bottom-large">
-        <div class="navigation__container">
-          <?php
+      <nav class="navbar mb-5">
+        <?php
 
-            wp_nav_menu([
-              'container' => 'div',
-              'menu_class' => 'navigation__menu',
-              'theme_location' => 'main-menu',
-            ]);
+          wp_nav_menu(array(
+            'theme_location' => 'main-menu',
+            'depth' => 0,
+            'container' => false,
+            'menu_class' => 'navbar-menu',
+            'menu_id' => 'primary-menu',
+            'walker' => new Hasnain\Walkers\Bulma(),
+          ));
 
-          ?>
-        </div>
+        ?>
       </nav>
-      <p class="text-center">
-        <span class="font-size-3-quaters">
+      <p class="has-text-centered">
+        <span class="is-size-7">
           <?php _e('Made with 💖 Design inspired by', 'hasnain'); ?>
           <a href="https://gomakethings.com" title="Go Make Things is a website that is focused on making vanilla JavaScript easy to learn">
             <?php _e('GoMakeThings', 'hasnain'); ?>
--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <body <?php body_class('transition'); ?>>
 
   <header class="mb-6">
-    <nav class="navbar theme-container--large" role="navigation" aria-label="main navigation">
+    <nav class="navbar theme-container theme-container--large" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="<?php echo site_url(); ?>">
           <img src="<?php echo get_template_directory_uri() . '/static/images/logo-1.png'; ?>" />
@@ -25,19 +25,20 @@
         </a>
       </div>
       <?php
+
         wp_nav_menu(array(
           'theme_location' => 'main-menu',
           'depth' => 0,
           'container' => false,
           'menu_class' => 'navbar-menu',
           'menu_id' => 'primary-menu',
-          'after' => "</div>",
           'walker' => new Hasnain\Walkers\Bulma(),
         ));
+
       ?>
     </nav>
   </header>
 
-  <main class="theme-container clearfix">
+  <main class="theme-container mb-6">
 
     <div class="slide"></div>
